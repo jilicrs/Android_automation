fea0885804f45d05b893fc92b1aa3d5d3ab483d7
risheng.chen <risheng.chen@lango-tech.com>
2023-03-27 16:52:36 +0800

    2023年3月27日 16：52 添加mic3308Windows检测功能

--- ProwiseSourceTouch/ProwiseSourceTouchTest.py
+++ ProwiseSourceTouch/ProwiseSourceTouchTest.py
@@ -5,14 +5,14 @@
 # @File      :ProwiseSourceTouchTest.py
 __version__ = '1.0.0'
 
-import os
 import sys
 import datetime
 import time
-import threading
+from ProwiseSourceTouch.MonitorMicarray import monitor_micarray
 from BasePage.Rs232_Connect import serial_sent_hex
 from ProwiseSourceTouch.MonitorUSB import monitor_disk
 from ProwiseSourceTouch.MonitorMouse import listen_mouse_click
+from ProwiseSourceTouch.thread import MyThread
 
 
 def waite():
@@ -25,68 +25,112 @@ def waite():
 
 
 if __name__ == '__main__':
-    # 测试设备ip：
-    adb_connect = input('①Input device adb ip to open Autoclick property:')
-    os.system('adb connect {}'.format(adb_connect))
-
     # 初始测试次数：
-    start_test = int(1)
-    # 最大测试次数：
-    end_test = int(input('②Number of test end:'))
-
+    start_test = 1
+    # 测试成功次数
     pass_test = 0
+    # 测试失败次数
     false_test = 0
+    # USB识别失败次数
+    usb_test_false = 0
+    # Mic识别错误次数
+    mic_test_false = 0
+    # 鼠标点击失败次数
+    mouse_test_click_false = 0
 
-    print('输入测试外部通道，大写：')  # 输入测试外部通道，大写：列如：USB_C
-    Source = input('③Test change source:')
+    while True:
+        # 最大测试次数：
+        end_test = input('①Number of test end:')
+        if end_test.isdigit():
+            end_test = int(end_test)
+            print('输入测试外部通道，大写：')  # 输入测试外部通道，大写：列如：USB_C
+            Source = input('②Test change source:')
+            if Source == 'HDMI1' or Source == 'HDMI2' or Source == 'HDMI3' or Source == 'HDMI4' or Source == 'OPS' \
+                    or Source == 'USB_C' or Source == 'USB_C_F' or Source == 'ANDROID' or Source == 'VGA' \
+                    or Source == 'AV':
+                while start_test < end_test:
+                    print('{}:start {} test'.format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), start_test))
+                    print('PASS:{}，False:{}'.format(pass_test, false_test))
+                    time.sleep(2)
+                    print('************1.{}:change {} source************'.format(datetime.datetime.now().
+                                                                                 strftime('%Y-%m-%d %H:%M:%S'),
+                                                                                 Source))
+                    # USB_C
+                    command_hex1 = serial_sent_hex(Source)
+                    time.sleep(2)
+                    print('************2.{}:Click test************'.format(datetime.datetime.now().strftime
+                                                                           ('%Y-%m-%d %H:%M:%S')))
+                    time.sleep(1)
+                    # 创建mouse_click_thread、mic_device_thread 和 usb_device_thread 线程对象
+                    mouse_click_thread = MyThread(target=listen_mouse_click)
+                    usb_device_thread = MyThread(target=monitor_disk)
+                    mic_device_thread = MyThread(target=monitor_micarray)
 
-    time.sleep(1)
-    os.system('adb -s {} shell setprop sys.xbh.sourcetouchautosend.start true'.format(adb_connect))
+                    # 启动mouse_click_thread 和 usb_device_thread 线程
+                    mouse_click_thread.start()
+                    usb_device_thread.start()
+                    mic_device_thread.start()
 
-    while start_test < end_test:
-        print('{}:start {} test'.format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), start_test))
-        print('Total test：{}，PASS:{},False:{}'.format(start_test, pass_test, false_test))
-        time.sleep(2)
-        print('************1.{}:change {} source************'.format(datetime.datetime.now().
-                                                                     strftime('%Y-%m-%d %H:%M:%S'),
-                                                                     Source))
-        # USB_C
-        command_hex1 = serial_sent_hex(Source)
-        time.sleep(2)
-        print('************2.{}:Click test************'.format(datetime.datetime.now().strftime
-                                                               ('%Y-%m-%d %H:%M:%S')))
-        time.sleep(1)
-        # mouse.position(400, 300)
-        mouse_click_thread = threading.Thread(target=listen_mouse_click)
-        usb_device_thread = threading.Thread(target=monitor_disk)
-        mouse_click_thread.start()
-        usb_device_thread.start()
-        if mouse_click_thread and usb_device_thread:
-            time.sleep(2)
-            print('==============================================================================================')
-            print('Test Result：{}:The {} test pass'.format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                                                           start_test), mouse_click_thread, usb_device_thread)
-            print('==============================================================================================')
-            pass_test += 1
-            start_test = start_test + 1
-            time.sleep(2)
-            print('3.{}:change android'.format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
-            command_hex2 = serial_sent_hex(command='ANDROID')
-            time.sleep(3)
+                    # join() 等待线程执行完毕在进行下一步
+                    mouse_click_thread.join()
+                    usb_device_thread.join()
+                    mic_device_thread.join()
+
+                    # 获取鼠标事件与USB检测事件结果：True or False
+                    mouse_result = mouse_click_thread.get_result()
+                    usb_result = usb_device_thread.get_result()
+                    mic_result = mic_device_thread.get_result()
+
+                    print(mouse_result, usb_result, mic_result)
+
+                    if mouse_result == True and usb_result == True and mic_result == True:
+                        time.sleep(2)
+                        print('==========================================================================='
+                              '===================')
+                        print('Test Result：{}:The {} test pass'.format(datetime.datetime.now().strftime
+                              ('%Y-%m-%d %H:%M:%S'), start_test), mouse_click_thread, usb_device_thread,
+                              mic_device_thread)
+                        print('=========================================================================='
+                              '====================')
+                        pass_test += 1
+                        start_test = start_test + 1
+                        time.sleep(2)
+                        print('3.{}:change android'.format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+                        # command_hex2 = serial_sent_hex(command='ANDROID')
+                        time.sleep(3)
+                        continue
+                    elif not mouse_result:
+                        time.sleep(2)
+                        print('{}: The {} test times Error:Coordinates not clicked!!!'.format
+                              (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), start_test))
+                        false_test += 1
+                        mouse_test_click_false += 1
+                        start_test += 1
+                        continue
+                    elif not usb_result:
+                        print('{}: The {} test times Error:USB device identification error!!!'.format
+                              (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), start_test))
+                        false_test += 1
+                        usb_test_false += 1
+                        start_test += 1
+                        continue
+                    elif not mic_result:
+                        print('{}: The {} test times Error:Mic device identification error!!!'.format
+                              (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), start_test))
+                        false_test += 1
+                        mic_test_false += 1
+                        start_test += 1
+                        continue
+                    elif start_test > end_test:
+                        print('{}:Total test {} times; Test PASS {} times, test False {} times'.format
+                              (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), end_test, pass_test, false_test))
+                        print('mouse click test fail {} times, USB device test fail {} times, Mic device test fail'
+                              '{} times'.format(mouse_test_click_false, usb_test_false, mic_test_false))
+                        input('============Press Enter to Exit============')
+                        break
+            else:
+                print(r'\\\\\\\\\\\\\\\\\\\The channel is not supported or the input is incorrect//////////////////')
+                continue
+        else:
+            print(r'\\\\\\\\\\\\\\\\\\\\\\\\\Need to enter integer/////////////////////////////')
             continue
-        elif not mouse_click_thread:
-            time.sleep(2)
-            print('{}: The {} test times Error:Coordinates not clicked!!!'.format(datetime.datetime.now().strftime
-                                                                                  ('%Y-%m-%d %H:%M:%S'), start_test))
-            false_test += 1
-            continue
-        elif not usb_device_thread:
-            print('{}: The {} test times Error:USB device identification error!!!'.format
-                  (datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), start_test))
-            false_test += 1
-            continue
-        elif start_test > end_test:
-            print('{}:Test PASS, test {} times'.format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                                                       end_test))
-            input('============Press Enter to Exit============')
-            break
