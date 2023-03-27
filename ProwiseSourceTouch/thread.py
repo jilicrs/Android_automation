6db4f654f4ae1302bcf50c2bff02f7c59ae31d46
risheng.chen <risheng.chen@lango-tech.com>
2023-03-27 16:59:23 +0800

    2023年3月27日 16：59 修改线程实现方式，增加获取线程接口返回值函数

---
+++ ProwiseSourceTouch/thread.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time      :2023/3/18 10:24
+# @Author    :risheng.chen@lango-tech.com
+# @File      :thread.py
+__version__ = '1.0.0'
+
+import threading
+from ProwiseSourceTouch.MonitorMouse import listen_mouse_click
+from ProwiseSourceTouch.MonitorUSB import monitor_disk
+
+
+class MyThread(threading.Thread):
+    def __init__(self, target=None, arges=()):
+        super(MyThread, self).__init__()
+        self.result = None
+        self.func = target
+        self.arges = arges
+
+    def run(self):
+        self.result = self.func(*self.arges)
+
+    def get_result(self):
+        try:
+            return self.result
+        except Exception as e:
+            print(e)
+            return None
+
+
+if __name__ == '__main__':
+    t1 = MyThread(target=listen_mouse_click)
+    t2 = MyThread(target=monitor_disk)
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+    result = t1.get_result()
+    res2 = t2.get_result()
+    print("mouse click", result)
+    print("u disk", res2)
